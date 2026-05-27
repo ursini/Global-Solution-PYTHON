1beba4f9aef9c9d9ae03a02f445f066d32a66284
ursini <leonardo.ursini2008@gmail.com>
2026-05-27 10:53:37 -0300

    chore: Adicionando o MENU

---
+++ gs.py
@@ -0,0 +1,12 @@
+while True:
+    print('--MENU--')
+
+    print('0 - Sobre o projeto')
+    print('1 - Registrar foco de incêndio')
+    print('2 - Ver focos ativos')
+    print('3 - Simular propagação')
+    print('4 - Calcular rota segura')
+    print('5 - Relatório operacional')
+    print('6 - Sair')
+
+    escolha = input('Qual o requisito: \n(1/2/3/4/5/6): ') 
