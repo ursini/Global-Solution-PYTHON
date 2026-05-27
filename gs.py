8955dc9225119e3c51ca551cfd664c4d452e7708
ursini <leonardo.ursini2008@gmail.com>
2026-05-27 10:56:08 -0300

    feat: Adicionando lista e primeira def

--- gs.py
+++ gs.py
@@ -1,3 +1,34 @@
+focos = []
+
+def registrar_foco(focos):
+
+    try:
+        latitude = float(input('Digite a latitude: '))
+        longitude = float(input('Digite a longitude: '))
+
+        while True:
+            intensidade = int(input('Digite a intensidade do fogo (1-10): '))
+
+            if intensidade < 1 or intensidade > 10:
+                print('Erro: intensidade deve estar entre 1 e 10!')
+            else:
+                break
+
+        vento = int(input('Digite a velocidade do vento: '))
+
+        foco = {
+            'latitude': latitude,
+            'longitude': longitude,
+            'intensidade': intensidade,
+            'vento': vento
+        }
+
+        focos.append(foco)
+
+        print('Foco registrado com sucesso!')
+
+    except ValueError:
+        print('Erro: digite apenas números válidos.')
 while True:
     print('--MENU--')
 
